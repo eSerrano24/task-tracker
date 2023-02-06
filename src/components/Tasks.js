b7635234d2ae1e16e2e8b1668f55e0b88eaf71cd
Eric <beatjunkieeric@gmail.com>
2023-02-05 19:36:00 -0800

    add tasks

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,8 +1,11 @@
-import { useState } from "react";
-
-const Tasks = () => {
-  const [tasks, setTasks] = useState();
-  return <div>Tasks</div>;
+const Tasks = ({ tasks }) => {
+  return (
+    <>
+      {tasks.map((task) => (
+        <h3 key={task.id}>{task.text} </h3>
+      ))}
+    </>
+  );
 };
 
 export default Tasks;
